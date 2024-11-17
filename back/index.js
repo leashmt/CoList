8473804f766ea8be14712d323a762ed9fb8afc3c
leaschmt <lea.schmitt@supdevinci-edu.fr>
2024-11-17 20:36:11 +0100

    feat: add CORS support for Socket.io connections

--- back/index.js
+++ back/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const cors = require('cors');
+
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+app.use(
+	cors({
+		origin: 'http://localhost:3000',
+		methods: ['GET', 'POST'],
+	})
+);
+const io = socketIo(server, {
+	cors: {
+		origin: 'http://localhost:3000', // Front-end autorisé
+		methods: ['GET', 'POST'], // Méthodes HTTP autorisées
+	},
+});
 
 io.on('connection', socket => {
 	console.log('A user connected');
