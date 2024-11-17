d2eadf4f338129381fc3f7ec198d51aaecfac62e
leaschmt <lea.schmitt@supdevinci-edu.fr>
2024-11-17 20:33:28 +0100

    feat: integrate Socket.io for real-time communication and update dependencies

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import io from 'socket.io-client';
+const socket = io('http://localhost:3001');
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	useEffect(() => {
+		socket.on('connect', () => {
+			console.log('Connected to server');
+		});
+
+		socket.on('disconnect', () => {
+			console.log('Disconnected from server');
+		});
+
+		return () => {
+			socket.off('connect');
+			socket.off('disconnect');
+		};
+	}, []);
+
+	return (
+		<div>
+			<h1>Socket.io Client</h1>
+		</div>
+	);
 }
 
 export default App;
